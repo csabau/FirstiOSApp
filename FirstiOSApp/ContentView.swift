99e972e7119353443facc45d2304f34f364e4a18
Caius <sabau.caius.93@gmail.com>
2022-02-24 19:43:04 +0000

    Initial Commit

---
+++ FirstiOSApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  FirstiOSApp
+//
+//  Created by Caius Sabau on 24/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isTextShowing = true
+    var body: some View {
+        VStack {
+            if isTextShowing {
+                Text("Hello, world!")
+                    .padding()
+            } else {
+                Text("")
+                    .padding()
+            }
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                isTextShowing.toggle()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstiOSApp/FirstiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstiOSAppApp.swift
+//  FirstiOSApp
+//
+//  Created by Caius Sabau on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
